10dcaa5cfba8c30326d96cdbd71eaccbdbc8dee7
Le_Van_Huy_Hoang <lvhhoangg171@gmail.com>
2024-08-18 09:52:51 +0700

    mongodb aggregate

--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -1,6 +1,8 @@
 import Joi from 'joi'
 import { StatusCodes } from 'http-status-codes'
 import ApiError from '~/utils/ApiError'
+import { BOARD_TYPES } from '~/utils/constants'
+
 
 
 const createNew = async (req, res, next) => {
@@ -18,7 +20,8 @@ const createNew = async (req, res, next) => {
       'string.min': 'Title should have a minimum length of 3',
       'string.max': 'Title should have a maximum length of 50'
     }),
-    description: Joi.string().required().min(3).max(256).trim().strict()
+    description: Joi.string().required().min(3).max(256).trim().strict(),
+    type: Joi.string().valid(BOARD_TYPES.PUBLIC, BOARD_TYPES.PRIVATE).required()
   })
 
   try {
