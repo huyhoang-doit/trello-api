d24806908e38c3baac6b8de990b5c59e93313f67
Le_Van_Huy_Hoang <lvhhoangg171@gmail.com>
2024-08-21 04:13:38 +0700

    add new column, card

---
+++ src/services/cardService.js
@@ -0,0 +1,29 @@
+import { cardModel } from '~/models/cardModel'
+import { columnModel } from '~/models/columnModel'
+
+
+
+const createNew = async (reqBody) => {
+  try {
+    const newCard = {
+      ...reqBody
+    }
+
+    const createdCard = await cardModel.createNew(newCard)
+
+    const getNewCard = await cardModel.findOneById(createdCard.insertedId)
+
+    if (getNewCard) {
+
+      // Cập nhận lại mảng cardOrderIds trong collectin columns
+      await columnModel.pushCardOrderIds(getNewCard)
+    }
+
+    return getNewCard
+  } catch (error) { throw error }
+}
+
+
+export const cardService = {
+  createNew
+}
