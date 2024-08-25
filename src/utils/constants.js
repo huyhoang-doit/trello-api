96451feaa78d1c35a60d3e3ac2fa6525ce99c50a
Le_Van_Huy_Hoang <lvhhoangg171@gmail.com>
2024-08-25 09:56:12 +0700

    update whitelist domains

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,8 @@
 // Những domain được phép truy cập đến tài nguyên của server
 export const WHITELIST_DOMAINS = [
-  // Không cần localhost FE nữa vì môi trường dev luôn được truy cập 
+  // Không cần localhost FE nữa vì môi trường dev luôn được truy cập
+  // domain deploy Vercel
+  'https://trello-web-one-eta.vercel.app/'
 ]
 
 export const BOARD_TYPES = {
